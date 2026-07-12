87125615235b92c7c39b1526d0a86d09f8b23294
Ojas Rane <98323556+OjasRane@users.noreply.github.com>
2026-07-12 22:41:56 +0530

    Revert "Removed find max element in svd_from_scratch() function" This commit changed the normalization factor from max_element to 255.0, which made the code numerically stable only for images, not general matrices.

--- mathematical_foundation/svd_from_scratch.py
+++ mathematical_foundation/svd_from_scratch.py
@@ -2,7 +2,8 @@ import numpy as np
 
 def svd_from_scratch(A):
     m, n = A.shape
-    A = A.astype(np.float64) / 255.0
+    max_element = float(np.max(A))
+    A = A.astype(np.float64) / max_element
     AtA = A.T @ A
     eigenvalues, eigenvectors = np.linalg.eigh(AtA)
     eigenvalues = eigenvalues[::-1]
@@ -18,4 +19,4 @@ def svd_from_scratch(A):
         if singular_values[i] > 1e-10:
             U[:, i] = (A @ np.array(V[:, i]).T) / singular_values[i]
 
-    return U, S*255.0, V.T
+    return U, S*max_element, V.T
